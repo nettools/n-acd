93f74f2252db9f8e8af5ba1a9fc0182132c236fc
David Herrmann <dh.herrmann@gmail.com>
2016-11-29 13:57:00 +0100

    test-api: add constructor/destructor symbol test
    Add the symbol-visibility tests for constructors/destructors to
    test-api.c.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/test-api.c
+++ src/test-api.c
@@ -10,6 +10,21 @@
 #include <string.h>
 #include "n-acd.h"
 
+static void test_api(void) {
+        NAcd *acd = NULL;
+        int r;
+
+        n_acd_unrefp(&acd);
+
+        r = n_acd_new(&acd);
+        assert(r >= 0);
+        n_acd_ref(acd);
+        n_acd_unref(acd);
+
+        n_acd_unref(acd);
+}
+
 int main(int argc, char **argv) {
+        test_api();
         return 0;
 }
