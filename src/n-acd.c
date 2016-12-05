2d1b5031b143242ce3631b19d99c9ee57b800e85
David Herrmann <dh.herrmann@gmail.com>
2016-12-06 00:05:21 +0100

    n-acd: filter too big messages via BPF
    Change the BPF filter to only let messages through that exactly match
    the size we expect. There is no point in allowing trailing bytes, since
    we do not know the intention.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/n-acd.c
+++ src/n-acd.c
@@ -331,7 +331,7 @@ static int n_acd_bind_socket(NAcd *acd, int s) {
                  * wire type, protocol type, and address lengths are correct.
                  */
                 BPF_STMT(BPF_LD + BPF_W + BPF_LEN, 0),                                                          /* A <- packet length */
-                BPF_JUMP(BPF_JMP + BPF_JGE + BPF_K, sizeof(struct ether_arp), 1, 0),                            /* packet >= arp packet ? */
+                BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, sizeof(struct ether_arp), 1, 0),                            /* packet == arp packet ? */
                 BPF_STMT(BPF_RET + BPF_K, 0),                                                                   /* ignore */
                 BPF_STMT(BPF_LD + BPF_H + BPF_ABS, offsetof(struct ether_arp, ea_hdr.ar_hrd)),                  /* A <- header */
                 BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, ARPHRD_ETHER, 1, 0),                                        /* header == ethernet ? */
