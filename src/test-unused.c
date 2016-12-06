edba3eb6650c05b3da9116f0529e3cb356610bad
David Herrmann <dh.herrmann@gmail.com>
2016-12-06 01:12:45 +0100

    test: add basic ACD test run with unused address
    Add test-unused.c which runs ACD on a veth link for an unused address.
    That is, the ACD engine should run smoothly without reporting any error.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

---
+++ src/test-unused.c
@@ -0,0 +1,61 @@
+/*
+ * Test with unused address
+ * Run the ACD engine with an address that is not used by anyone else on the
+ * link. This should just pass through, with a short, random timeout.
+ */
+
+#include <stdlib.h>
+#include "test.h"
+
+static void test_unused_fn(NAcd *acd, void *userdata, unsigned int event, const struct ether_arp *conflict) {
+        bool *running = userdata;
+
+        assert(event == N_ACD_EVENT_READY);
+        *running = false;
+}
+
+static void test_unused(int ifindex, const struct ether_addr *mac) {
+        struct pollfd pfds;
+        bool running;
+        NAcd *acd;
+        int r, fd;
+
+        r = n_acd_new(&acd);
+        assert(r >= 0);
+
+        r = n_acd_set_ifindex(acd, ifindex);
+        assert(r >= 0);
+        r = n_acd_set_mac(acd, mac);
+        assert(r >= 0);
+        r = n_acd_set_ip(acd, &(struct in_addr){ htons((192 << 24) | (168 << 16) | (1 << 0)) });
+        assert(r >= 0);
+
+        n_acd_get_fd(acd, &fd);
+        r = n_acd_start(acd, test_unused_fn, &running);
+        assert(r >= 0);
+
+        for (running = true; running; ) {
+                pfds = (struct pollfd){ .fd = fd, .events = POLLIN };
+                r = poll(&pfds, 1, -1);
+                assert(r >= 0);
+
+                r = n_acd_dispatch(acd);
+                assert(r >= 0);
+        }
+
+        n_acd_unref(acd);
+}
+
+int main(int argc, char **argv) {
+        struct ether_addr mac;
+        int r, ifindex;
+
+        r = test_setup();
+        if (r)
+                return r;
+
+        test_veth_new(&ifindex, &mac, NULL, NULL);
+        test_unused(ifindex, &mac);
+
+        return 0;
+}
--- src/test.h
+++ src/test.h
@@ -12,8 +12,71 @@
 #include <net/ethernet.h>
 #include <net/if.h>
 #include <netinet/in.h>
+#include <poll.h>
 #include <sched.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include "n-acd.h"
+
+static inline void test_veth_new(int *parent_indexp,
+                                 struct ether_addr *parent_macp,
+                                 int *child_indexp,
+                                 struct ether_addr *child_macp) {
+        struct ifreq ifr;
+        int r, s;
+
+        /* Eww... but it works. */
+        r = system("ip link add type veth");
+        assert(r == 0);
+        r = system("ip link set veth0 up");
+        assert(r == 0);
+        r = system("ip link set veth1 up");
+        assert(r == 0);
+
+        s = socket(AF_INET, SOCK_DGRAM, 0);
+        assert(s >= 0);
+
+        if (parent_indexp) {
+                *parent_indexp = if_nametoindex("veth0");
+                assert(*parent_indexp > 0);
+        }
+
+        if (child_indexp) {
+                *child_indexp = if_nametoindex("veth1");
+                assert(*child_indexp > 0);
+        }
+
+        if (parent_macp) {
+                memset(&ifr, 0, sizeof(ifr));
+                strcpy(ifr.ifr_name, "veth0");
+                r = ioctl(s, SIOCGIFHWADDR, &ifr);
+                assert(r >= 0);
+                memcpy(parent_macp->ether_addr_octet, ifr.ifr_hwaddr.sa_data, ETH_ALEN);
+        }
+
+        if (child_macp) {
+                memset(&ifr, 0, sizeof(ifr));
+                strcpy(ifr.ifr_name, "veth1");
+                r = ioctl(s, SIOCGIFHWADDR, &ifr);
+                assert(r >= 0);
+                memcpy(child_macp->ether_addr_octet, ifr.ifr_hwaddr.sa_data, ETH_ALEN);
+        }
+
+        close(s);
+}
+
+static inline int test_setup(void) {
+        int r;
+
+        r = unshare(CLONE_NEWNET);
+        if (r < 0) {
+                assert(errno == EPERM);
+                return 77;
+        }
+
+        return 0;
+}
